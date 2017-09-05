66b5fb9ae89087c00d2b08da5323d728e05c58f2
rugbyprof <terry.griffin@mwsu.edu>
2017-09-05 15:06:21 -0500

    added flask app

---
+++ Resources/flask_app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
