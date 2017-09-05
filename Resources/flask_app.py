19d08bceef456ef719280504da921d5cc7fad644
Terry Griffin <rugbyprof@users.noreply.github.com>
2017-09-05 15:21:40 -0500

    Update flask_app.py

--- Resources/flask_app.py
+++ Resources/flask_app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+"""
+$ export FLASK_APP=main.py
+$ export FLASK_DEBUG=1
+$ python -m flask run --host 0.0.0.0
+"""
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello World! This is aweseme!!"
 
 
 @app.route('/<name>')
